f4e138575916e1e99aa99420d47effd768da72d9
FernandoCruzVelazquez <fernandocruzv2003@outlook.com>
2026-04-28 15:47:21 -0600

    Fix: resolución de conflictos en rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,12 @@ import { LoaderComponent } from './componentes/loader/loader.component';
 import { PokedexComponent } from './componentes/pokedex/pokedex.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { LayoutComponent } from './componentes/layout/layout.component';
-
+ 
 export const routes: Routes = [
   { path: '', component: LoaderComponent },
-
+ 
   { path: 'login', component: LoginComponent },
-
+ 
   // Lo que va dentro del path de layout es lo que se muestra debajo del layout
   {
     path: '',
@@ -17,6 +17,6 @@ export const routes: Routes = [
       { path: 'pokedex', component: PokedexComponent },
     ]
   },
-
+  //Si hay rutas que no existen los mandamos al login por seguridad :p
   { path: '**', redirectTo: 'login' }
 ];
