a08ae08db7ee3dbb0c8be0f623dc2403d3e3e05c
FernandoCruzVelazquez <fernandocruzv2003@outlook.com>
2026-04-30 12:45:09 -0600

    Fix: resolución de conflictos en rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,18 +4,24 @@ import { PokedexComponent } from './componentes/pokedex/pokedex.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { LayoutComponent } from './componentes/layout/layout.component';
 import { DetallePokemonComponent } from './componentes/detalle-pokemon/detalle-pokemon.component';
+//import { UsuarioGetAllComponent } from './componentes/usuario-get-all/usuario-get-all.component';
  
 export const routes: Routes = [
   { path: '', component: LoaderComponent },
  
   { path: 'login', component: LoginComponent },
-
+ 
   { path: 'pokemon/:id', component: DetallePokemonComponent },
  
+  // Lo que va dentro del path de layout es lo que se muestra debajo del layout
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
       { path: 'pokedex', component: PokedexComponent },
-      {path: 'usuarios', component: UsuarioGetAllComponent},
+      //{path: 'usuarios', component: UsuarioGetAllComponent},
     ]
   },
-  //Si hay rutas que no existen los mandamos al login por seguridad :p
+ 
   { path: '**', redirectTo: 'login' }
 ];
